7177d2738e53169e3e71aff78c6822507516dff5
danieltoguti <danieltoguti@gmail.com>
2020-11-14 18:49:46 -0300

    pequena mudança

--- acerte_um_numero.py
+++ acerte_um_numero.py
@@ -36,6 +36,7 @@ class AcerteNumero:
                         if int(self.valor_do_chute) == self.valor_aleatorio:
                             self.tentar_novamente = False
                             print('Parabéns Você ganhou!')
+                            self.fecharJanela()
                             break
                         
         except :
